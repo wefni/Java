0a31c259e63558438a905d0cfeebd2aaea75cd5c
wefni <47455954+wefni@users.noreply.github.com>
2020-01-20 16:57:13 +0100

    C-Sharp
    C-Sharp

---
+++ eUtazas_Delutan_Erettsegi/eUtazas_Delutan_Erettsegi/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eUtazas_Delutan_Erettsegi
+{
+    public class Program
+    {
+        List<Utasadat> uList = new List<Utasadat>();
+
+        Program() {
+
+            Feladatok();
+        
+        }
+
+
+        public void Feladatok() {
+
+            Console.WriteLine("Első feladat: ");
+
+            try
+            {
+                foreach (string sor in File.ReadAllLines("utasadat.txt", Encoding.UTF8))
+                {
+                    uList.Add(new Utasadat(sor));
+                    
+                }
+                Console.WriteLine("Adatbeolvasva");
+            }
+            catch (FileNotFoundException fex)
+            {
+
+                Console.WriteLine("Fájl nem található!");
+            }
+            catch (Exception ex) {
+
+                Console.WriteLine($"Hiba: {ex.Message}");
+            
+            }
+
+
+            Console.WriteLine($"2.feladat:\nA buszra {uList.Count} utas akart felszállni!");
+            int db = 0;
+            foreach (Utasadat u in uList)
+            {
+                if (u.Lejart()) {
+                    db++;
+                
+                }
+
+            }
+
+            Console.WriteLine($"3.feladat: A buszra {db} utas nem szállhatott fel");
+
+            Console.WriteLine($"4.feladat: A legtöbb utas({} fő) a {}. megállóban szállt fel");
+
+            List<string> temp = new List<string>();
+
+            foreach  (Utasadat u in uList)
+            {
+                if (temp.Contains(u.ID))
+                {
+                    for (int i = 0; i < ; i++)
+                    {
+
+                    }
+
+                }
+
+
+            }
+
+
+        
+        }
+
+        static void Main(string[] args)
+        {
+            new Program();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ eUtazas_Delutan_Erettsegi/eUtazas_Delutan_Erettsegi/utasadat.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace eUtazas_Delutan_Erettsegi
+{
+    public class Utasadat
+    {
+
+        public int Sorszam { get; set; }
+        public string Felszall { get; set; }
+        public string ID { get; set; }
+        public string Tipus { get; set; }
+        public string Ervenyes { get; set; }
+
+        public Utasadat(string sor) {
+            int temp;
+            Int32.TryParse(sor.Split(' ')[0], out temp);
+            Sorszam = temp;
+
+            Felszall = sor.Split(' ')[1];
+            ID = sor.Split(' ')[2];
+            Tipus = sor.Split(' ')[3];
+            Ervenyes = sor.Split(' ')[4];
+
+
+        }
+
+        public bool Lejart() {
+
+            if (!Ervenyes.Equals("0"))
+            {
+                if (Ervenyes.Length == 8) {
+
+                    DateTime erv = DateTime.ParseExact(Ervenyes, "yyyyMMdd", CultureInfo.InvariantCulture);
+                    DateTime menet = DateTime.ParseExact(Felszall.Split('-')[0], "yyyyMMdd", CultureInfo.InvariantCulture);
+
+                    if (erv < menet)
+                    {
+                        return true;
+                    }
+                }
+            }
+            else {
+                return true;
+            
+            }
+            return false;
+        }
+
+
+
+    }
+}
